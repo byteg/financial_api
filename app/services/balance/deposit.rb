16a5206d2a1e387bca03c58d07272c5c161898b5
Denis Kuznetcov <denis@f7.ru>
2025-05-20 20:36:30 +0400

    removed not needed lock

--- app/services/balance/deposit.rb
+++ app/services/balance/deposit.rb
@@ -5,10 +5,8 @@ class Balance::Deposit
   end
 
   def call
+    raise ArgumentError, "Amount must be greater than 0" if @amount_cents <= 0
     User.transaction do
-      @user.lock!
-      raise ArgumentError, "Amount must be greater than 0" if @amount_cents <= 0
-      
       @user.increment!(:amount_cents, @amount_cents)
       @user.balance_transactions.create!(amount_cents: @amount_cents)
     end
